27ab144956d936c0da3861cbdd1d9be515f8116b
연어쏘는초밥 <hyeongseoksim@gmail.com>
2025-02-23 18:48:41 +0900

    Update Practice jsx
    - Update Naming Convention
        - Start with upper letter

--- src/pages/practice.jsx
+++ src/pages/practice.jsx
@@ -1,16 +1,49 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import '../App.css';
 import './pages.css';
 
-const practice = () => {
-    const navigate = Navigate();
+const Practice = () => {
+    const navigate = useNavigate();
+    const timerRef = useState(null);
+    const [recordTime, setRecordTime] = useState(0);
+
+    const startTimer = () => {
+        setRecordTime(0);
+        timerRef.current = setInterval(() => {
+          setRecordTime((prevTime) => {
+            const newTime = prevTime + 1;
+            if (newTime >= 60) {
+              clearInterval(timerRef.current);
+            }
+            return newTime;
+          });
+        }, 1000);
+      };
+    
+      const handleStart = () => {
+        startTimer();
+        runPython();
+      };
+    
+      const runPython = async () => {
+        try {
+          const response = await fetch('http://localhost:5000/run-python');
+          const data = await response.text()
+          console.log(data);
+        } catch (error) {
+          console.error('Error:', error);
+          alert('실행 중 에러 발생');
+        }
+      };
 
     return(
         <div>
-            
+            <button className="startButton" onClick={handleStart}>
+                Start Now!
+            </button>
         </div>
     );
 };
 
-export default practice;
+export default Practice;
